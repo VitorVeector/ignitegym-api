e777f67de1dace2966bc181b2136fecc6e00b162
Vitor Hugo <job_vitoraraujo@outlook.com>
2023-03-03 07:55:11 -0300

    :wrench: test: Change expires token time

--- src/providers/GenerateRefreshToken.js
+++ src/providers/GenerateRefreshToken.js
@@ -3,7 +3,7 @@ const dayjs = require("dayjs");
 
 class GenerateRefreshToken {
   async execute(userId, newToken) {
-    const expires_in = dayjs().add(15, "second").unix();
+    const expires_in = dayjs().add(1, "day").unix();
 
     await knex("users_tokens").insert({
       user_id: userId,
